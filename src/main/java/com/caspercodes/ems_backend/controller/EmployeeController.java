6adea7b3972a80bb73d43ec341d811c5ea4da53f
Oluwasanumi <oluwasanumibalogun@gmail.com>
2025-03-07 00:49:29 +0100

    Test Debug

--- src/main/java/com/caspercodes/ems_backend/controller/EmployeeController.java
+++ src/main/java/com/caspercodes/ems_backend/controller/EmployeeController.java
@@ -50,5 +50,6 @@ public class EmployeeController {
     public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
         employeeService.deleteEmployee(employeeId);
         return ResponseEntity.ok("Employee deleted successfully!");
+
     }
 }
